695b149c34b30631784140cb9b3c623696b26e99
Kaden Thielmann <kadenthielmann@gmail.com>
2020-10-30 16:40:24 -0400

    Fixed bug appearing after pressing equals and improved UX

--- js/calculator.js
+++ js/calculator.js
@@ -1,90 +1,88 @@
-let currDisplay = "";
-let prevDisplay = "";
-let pressedOperand = false;
-let prevOperand = "";
+let currDisplay, prevDisplay, pressedOperand, prevOperand;
 
 const operands = Array.from(document.getElementsByClassName("operand"));
 const operators = Array.from(document.getElementsByClassName("operator"));
-
 const display = document.querySelector("[value='screen']");
-
-display.textContent = "";
 
 operands.forEach(key => key.addEventListener("click", updateScreen))
 operators.forEach(key => key.addEventListener("click", operate))
 
+initValues();
+
+function initValues() {
+    currDisplay = "";
+    prevDisplay = "";
+    pressedOperand = false;
+    prevOperand = "";
+    display.textContent = "0";
+}
+
 function updateScreen(e) {
     let input = e.target.textContent;
-    currDisplay = Number(display.textContent);
-    currScreen = currDisplay;
+    currScreen = Number(display.textContent);
     if (pressedOperand == true) {
         display.textContent = "";
     }
     if (input == "AC") {
-        display.textContent = "";
-        currDisplay = "";
-        prevDisplay = "";
-        prevOperand = "";
+        initValues();
     }
-    else if (input == "C") {
-        let currScreen = display.textContent.toString()
+    else if (input == "DEL") {
+        let currScreen = display.textContent.toString();
         if (currScreen.length > 0) display.textContent = currScreen.slice(0, -1);
     }
-    else if (input == "+/-") display.textContent = -currDisplay;
+    else if (input == "±") display.textContent = -currScreen;
     else if (input == ".") {
-        if (display.textContent.toString().indexOf(".") <0) display.textContent += input;
+        if (display.textContent.toString().indexOf(".") < 0 &&
+            display.textContent.toString().length < 13)
+            display.textContent += input;
     }
-    else display.textContent += input;
+    else {
+        if (display.textContent.toString().length < 13)
+            display.textContent = Number(display.textContent + input);
+    }
     currDisplay = Number(display.textContent);
+    if (display.textContent.toString().length < 1) display.textContent = 0;
     if (isNaN(currDisplay)) currDisplay = "";
     pressedOperand = false;
 }
 
 function operate(e) {
     const operation = e.target.dataset.val;
-    switch (prevOperand) {
+    currDisplay = Number(display.textContent);
+    performOp(prevOperand);
+    handleLongNumbers()
+    display.textContent = currDisplay;
+    prevOperand = operation;
+    prevDisplay = currDisplay;
+    pressedOperand = true;
+}
+
+function handleLongNumbers() {
+    if (currDisplay > 9999999999999) currDisplay = "Too large."
+    else if (currDisplay < -9999999999999) currDisplay = "Too small."
+    if (currDisplay.toString().length > 13) currDisplay = currDisplay.toString().slice(0, 13)
+}
+
+function performOp(op) {
+    switch (op) {
         case "add":
-            currDisplay = Number(display.textContent);
             currDisplay += Number(prevDisplay);
-            display.textContent = currDisplay;
-            prevDisplay = currDisplay;
-            prevOperand = operation;
             break;
         case "subtract":
-            currDisplay = Number(display.textContent);
             currDisplay = Number(prevDisplay) - currDisplay;
-            display.textContent = currDisplay;
-            prevDisplay = currDisplay;
-            prevOperand = operation;
             break;
         case "multiply":
-            currDisplay = Number(display.textContent);
             currDisplay *= Number(prevDisplay);
-            display.textContent = currDisplay;
-            prevDisplay = currDisplay;
-            prevOperand = operation;
             break;
         case "divide":
-            currDisplay = Number(display.textContent);
             if (currDisplay == 0) {
                 alert("Never divide by zero!")
                 break;
             }
-            currDisplay = Number(prevDisplay)/currDisplay;
-            display.textContent = currDisplay;
-            prevDisplay = currDisplay;
-            prevOperand = operation;
+            currDisplay = Number(prevDisplay) / currDisplay;
             break;
         case "equals":
-            display.textContent = currDisplay;
             prevDisplay = Number(display.textContent);
-            prevOperand = "";
             break;
-        default:
-            prevOperand = operation;
-            currDisplay = Number(display.textContent);
-            prevDisplay = currDisplay;
     }
-    console.log(prevOperand)
-    pressedOperand = true;
 }
